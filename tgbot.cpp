22a096a0da389c1b638e85d6778c220767eabfbe
illusiOxd <123993174+illusiOxd@users.noreply.github.com>
2024-02-20 15:11:39 +0200

    Update tgbot.cpp

--- tgbot.cpp
+++ tgbot.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <fstream>
 #include <iostream>
+#include <map>
 
 using namespace TgBot;
 using namespace std;
@@ -11,40 +12,38 @@ class TgUser {
 private:
     string name;
     int age, password, ID;
+    bool isRegistered;
 
 public:
-    TgUser()
-    {
+    TgUser() {
         name = " ";
         age = -1;
         password = -1;
+        isRegistered = false;
     }
 
-    string GetName() const
-    {
+    string GetName() const {
         return name;
     }
-    void SetName(string newname)
-    {
+    void SetName(string newname) {
         this->name = newname;
     }
 
-    int GetAge() const
-    {
+    int GetAge() const {
         return age;
     }
-    void SetAge(int newage)
-    {
+    void SetAge(int newage) {
         this->age = newage;
     }
 
-    int GetPassword() const
-    {
+    int GetPassword() const {
         return password;
     }
-    void SetPassword(int newPassword)
-    {
+    void SetPassword(int newPassword) {
         this->password = newPassword;
+    }
+    bool IsRegistered() const {
+        return isRegistered;
     }
 };
 
@@ -69,239 +68,205 @@ InlineKeyboardMarkup::Ptr createInlineYesNoKeyboard() {
     return keyboard;
 }
 
-//if (inFile.is_open()) {
-//    string line;
-//    while (getline(inFile, line)) {
-//        if (line.find("TG username: " + message->from->username) != string::npos) {
-//            // Если нашли юзернейм, ставим флаг и выходим из цикла
-//            usernameExists = true;
-//            break;
-//        }
-//    }
-//    inFile.close();
-//}
+ReplyKeyboardMarkup::Ptr createBottomKeyboard() {
+    ReplyKeyboardMarkup::Ptr keyboard(new ReplyKeyboardMarkup);
+    vector<KeyboardButton::Ptr> row1, row2;
 
-int main()
-{
+    KeyboardButton::Ptr button1(new KeyboardButton);
+    button1->text = "/start";
+    row1.push_back(button1);
+
+    KeyboardButton::Ptr button2(new KeyboardButton);
+    button2->text = "/info";
+    row1.push_back(button2);
+
+    KeyboardButton::Ptr button3(new KeyboardButton);
+    button3->text = "/links";
+    row2.push_back(button3);
+
+    KeyboardButton::Ptr button4(new KeyboardButton);
+    button4->text = "/writefile";
+    row2.push_back(button4);
+
+    keyboard->keyboard.push_back(row1);
+    keyboard->keyboard.push_back(row2);
+
+    keyboard->resizeKeyboard = true;
+    keyboard->oneTimeKeyboard = false;
+    keyboard->selective = false;
+
+    return keyboard;
+}
+
+map<int, TgUser> usersData;
+
+int main() {
     Bot bot("6927127350:AAGWg0O3VpP3e0o0HNySeW81SgRbUYwmoPY");
-    TgUser PersonalUser;
+    map<int, TgUser> userMap;
+
     bool isRegistered = false;
     bool isWaitingForName = false, isWaitingForAge = false, isWaitingForPass = false;
-    TgBot::BotCommand::Ptr startCommand(new TgBot::BotCommand);
-    startCommand->command = "/start";
-    startCommand->description = "Start the bot";
-    bot.getEvents().onCommand("start", [&bot, &isRegistered, &PersonalUser](Message::Ptr message) {
-        // Отправляем приветственное сообщение
-        bot.getApi().sendMessage(message->chat->id, "Hello! I am a test bot. \n /info to watch your info after registration \n /links to watch my github");
 
-        // Открываем файл для чтения
-        ifstream inFile("user_data.txt");
+    bot.getEvents().onCommand("start", [&bot, &isRegistered, &userMap](Message::Ptr message) {
+        int userId = message->from->id;
 
-        bool userFound = false;
+        TgUser PersonalUser;
+
+        string filename = "user_" + to_string(userId) + "_" + message->from->username.c_str() + "_data.txt";
+        ifstream inFile(filename);
+
         if (inFile.is_open()) {
-            string line; // Флаг, указывающий, найден ли пользователь
-            vector<string> fileLines; 
-
-            // Просматриваем файл построчно
+            isRegistered = true;
+            string line;
             while (getline(inFile, line)) {
-                fileLines.push_back(line);
-            }
-
-            // Второй проход: обрабатываем данные
-            for (const string& fileLine : fileLines) {
-                if (fileLine.find("TG username: " + message->from->username) != string::npos) {
-                    isRegistered = true; // Устанавливаем флаг регистрации
-
-                    // Перебираем строки, начиная с текущей, чтобы найти данные о пользователе
-                    for (size_t i = 0; i < fileLines.size(); ++i) {
-                        if (fileLines[i].find("Name: ") != string::npos) {
-                            string usernametemp = fileLines[i].substr(6); // 6 - длина "Name: "
-                            PersonalUser.SetName(usernametemp);
-                        }
-                        else if (fileLines[i].find("Age: ") != string::npos) {
-                            PersonalUser.SetAge(stoi(fileLines[i].substr(5))); // 5 - длина "Age: "
-                        }
-                        else if (fileLines[i].find("Password: ") != string::npos) {
-                            PersonalUser.SetPassword(stoi(fileLines[i].substr(10))); // 10 - длина "Password: "
-                        }
-                    }
-
-                    userFound = true; // Устанавливаем флаг найденного пользователя
-                    break;
+                if (line.find("Name: ") != string::npos) {
+                    PersonalUser.SetName(line.substr(6));
+                }
+                else if (line.find("Age: ") != string::npos) {
+                    PersonalUser.SetAge(stoi(line.substr(5)));
+                }
+                else if (line.find("Password: ") != string::npos) {
+                    PersonalUser.SetPassword(stoi(line.substr(10)));
                 }
             }
-            // Закрываем файл
+
+            userMap[userId] = PersonalUser;
+
             inFile.close();
-        }
 
-        // Если пользователь не найден, отправляем сообщение о регистрации
-        if (!userFound) {
-            bot.getApi().sendMessage(message->chat->id, "Do you want to register? (Yes/No)");
-        }
-        });
-
-    bot.getEvents().onCommand("links", [&bot, &PersonalUser](Message::Ptr message)
-        {
-            TgBot::InlineKeyboardMarkup::Ptr keyboard(new TgBot::InlineKeyboardMarkup);
-            TgBot::InlineKeyboardButton::Ptr button(new TgBot::InlineKeyboardButton);
-
-            button->text = "Github";
-            button->url = "https://github.com/illusiOxd";
-            keyboard->inlineKeyboard.push_back({ button });
-
-            bot.getApi().sendMessage(message->chat->id, "Click that buttons to follow the link.", false, 0, keyboard);
-        });
-
-    bot.getEvents().onAnyMessage([&bot, &PersonalUser, &isRegistered, &isWaitingForName, &isWaitingForAge, &isWaitingForPass](Message::Ptr message) {
-        // Вывод информации о том, кто пишет боту
-        printf("Message from: %s\n", message->from->username.c_str());
-        printf("Text: %s\n", message->text.c_str());
-        if (!isRegistered) {
-            if (isWaitingForName) {
-                // Пользователь уже ввел имя
-                PersonalUser.SetName(message->text);
-                isWaitingForName = false; // Пользователь ввел имя, больше не ждем
-                bot.getApi().sendMessage(message->chat->id, "Type your age: ");
-                isWaitingForAge = true; // Ожидаем ввод возраста
+            // Проверяем наличие данных перед отправкой сообщения
+            if (userMap[userId].GetName() != " " && userMap[userId].GetAge() != -1 && userMap[userId].GetPassword() != -1) {
+                bot.getApi().sendMessage(message->chat->id, "Your data has been loaded from the file.", false, 0, createBottomKeyboard());
             }
-            else if (isWaitingForAge) {
-                // Пользователь уже ввел возраст
-                try {
-                    int age = stoi(message->text);
-                    PersonalUser.SetAge(age);
-                    isWaitingForAge = false; // Пользователь ввел возраст, больше не ждем 
-                    bot.getApi().sendMessage(message->chat->id, "Type your password: ");
-                    isWaitingForPass = true; // Ожидаем ввод пароля
-                }
-                catch (const invalid_argument& e) {
-                    bot.getApi().sendMessage(message->chat->id, "Please type a valid age.");
-                }
-            }
-            else if (isWaitingForPass) {
-                // Пользователь уже ввел пароль
-                try {
-                    int password = stoi(message->text);
-                    PersonalUser.SetPassword(password);
-                    isWaitingForPass = false; // Пользователь ввел пароль, больше не ждем
-                    isRegistered = true; // Регистрация завершена
-                    bot.getApi().sendMessage(message->chat->id, "Registration completed.  \n /info to watch your info after registration \n /links to watch my github \n /writefile to sync your profile data");
-
-
-                }
-                catch (const invalid_argument& e) {
-                    bot.getApi().sendMessage(message->chat->id, "Please type a valid password.");
-                }
-            }
-            else if (message->text == "Yes" || message->text == "yes") {
-                // Пользователь согласился зарегистрироваться
-                bot.getApi().sendMessage(message->chat->id, "Type your name: ");
-                isWaitingForName = true; // Ожидаем ввод имени
+            else {
+                bot.getApi().sendMessage(message->chat->id, "Your data is incomplete.", false, 0, createBottomKeyboard());
             }
         }
         else {
-            // Обработка вывода информации о пользователе
-            if (StringTools::startsWith(message->text, "/info")) {
-                string name = PersonalUser.GetName();
-                if (!name.empty()) {
-                    bot.getApi().sendMessage(message->chat->id, "Your name: " + name);
-                }
-                else {
-                    bot.getApi().sendMessage(message->chat->id, "Your name is not provided yet.");
-                }
-                bot.getApi().sendMessage(message->chat->id, "Your age: " + to_string(PersonalUser.GetAge()));
-                bot.getApi().sendMessage(message->chat->id, "Do you want to watch your password?", false, 0,
-                    createInlineYesNoKeyboard());
-            }
-        }
-        });
-
-    bot.getEvents().onCallbackQuery([&bot, &PersonalUser](CallbackQuery::Ptr query) {
-        string data = query->data;
-        string username = query->from->username;
-
-        if (data == "show_password") {
-            cout << "This user clicked 'Yes' button in password show process: " << username << endl;
-            // Пользователь согласился показать пароль
-            int password = PersonalUser.GetPassword();
-            bot.getApi().sendMessage(query->message->chat->id, "Your password: " + to_string(password));
-        }
-        else if (data == "no_show_password") {
-            cout << "This user clicked 'No' button in password show process: " << username << endl;
-            // Пользователь отказался показывать пароль
-            bot.getApi().sendMessage(query->message->chat->id, "Okay, I won't show you your password.");
-        }
-        });
-
-    bot.getEvents().onCommand("writefile", [&bot, &PersonalUser](Message::Ptr message)
-        {
-            ifstream inFile("user_data.txt");
-            bool usernameExists = false;
-
-            // Проверяем, есть ли уже юзернейм в файле
-            if (inFile.is_open()) {
-                string line;
-                while (getline(inFile, line)) {
-                    if (line.find("TG username: " + message->from->username) != string::npos) {
-                        // Если нашли юзернейм, ставим флаг и выходим из цикла
-                        usernameExists = true;
-                        break;
-                    }
-                }
-                inFile.close();
-            }
-
-            if (!usernameExists) {
-                // Если юзернейма еще нет в файле, производим запись
-                ofstream outFile("user_data.txt", ios::app);
-                if (outFile.is_open()) {
-                    // Определяем последний ID из файла
-                    int lastID = 0;
-                    ifstream inFile("user_data.txt");
-                    if (inFile.is_open()) {
-                        string line;
-                        while (getline(inFile, line)) {
-                            if (line.find("ID:") != string::npos) {
-                                int userID = stoi(line.substr(line.find(":") + 2));
-                                if (userID > lastID) {
-                                    lastID = userID;
-                                }
-                            }
-                        }
-                        inFile.close();
-                    }
-
-                    // Увеличиваем ID на 1, чтобы получить новый уникальный ID
-                    int newID = lastID + 1;
-
-                    // Записываем данные в файл
-                    outFile << "ID: " << setfill('0') << setw(4) << newID << endl; // Форматируем ID как 4-значное число с ведущими нулями
-                    outFile << "Name: " << PersonalUser.GetName() << endl;
-                    outFile << "TG username: " << message->from->username << endl;
-                    outFile << "Age: " << PersonalUser.GetAge() << endl;
-                    outFile << "Password: " << PersonalUser.GetPassword() << endl;
-                    outFile << "----------------------" << endl;
-                    bot.getApi().sendMessage(message->chat->id, "Your data is written.");
-
-                    outFile.close();
-                }
+            if (!isRegistered) {
+                bot.getApi().sendMessage(message->chat->id, "Do you want to register? (Yes/No)", false, 0, createBottomKeyboard());
             }
             else {
-                bot.getApi().sendMessage(message->chat->id, "Your username is already in the file.");
+                bot.getApi().sendMessage(message->chat->id, "You are already registered.", false, 0, createBottomKeyboard());
             }
+        }
         });
 
-    try
-    {
+
+    bot.getEvents().onAnyMessage([&bot, &userMap, &isRegistered, &isWaitingForName, &isWaitingForAge, &isWaitingForPass](Message::Ptr message) {
+        printf("Message from: %s\n", message->from->username.c_str());
+        printf("Text: %s\n", message->text.c_str());
+        int userId = message->from->id;
+
+        if (!isRegistered) {
+            if (isWaitingForName) {
+                userMap[userId].SetName(message->text);
+                isWaitingForName = false;
+                bot.getApi().sendMessage(message->chat->id, "Type your age: ", false, 0, createBottomKeyboard());
+                isWaitingForAge = true;
+            }
+            else if (isWaitingForAge) {
+                try {
+                    int age = stoi(message->text);
+                    userMap[userId].SetAge(age);
+                    isWaitingForAge = false;
+                    bot.getApi().sendMessage(message->chat->id, "Type your password (only numbers): ", false, 0, createBottomKeyboard());
+                    isWaitingForPass = true;
+                }
+                catch (const invalid_argument& e) {
+                    bot.getApi().sendMessage(message->chat->id, "Please type a valid age.", false, 0, createBottomKeyboard());
+                }
+            }
+            else if (isWaitingForPass) {
+                try {
+                    int password = stoi(message->text);
+                    userMap[userId].SetPassword(password);
+                    isWaitingForPass = false;
+                    isRegistered = true;
+                    bot.getApi().sendMessage(message->chat->id, "Registration completed.", false, 0, createBottomKeyboard());
+                }
+                catch (const invalid_argument& e) {
+                    bot.getApi().sendMessage(message->chat->id, "Please type a valid password.", false, 0, createBottomKeyboard());
+                }
+            }
+            else if (message->text == "Yes" || message->text == "yes") {
+                bot.getApi().sendMessage(message->chat->id, "Type your name (only english): ", false, 0, createBottomKeyboard());
+                isWaitingForName = true;
+            }
+        }
+        });
+
+    bot.getEvents().onCommand("info", [&bot, &userMap](Message::Ptr message) {
+        int userId = message->from->id;
+        string name = userMap[userId].GetName();
+
+        if (!name.empty()) {
+            bot.getApi().sendMessage(message->chat->id, "Your name: " + name, false, 0, createBottomKeyboard());
+        }
+        else {
+            bot.getApi().sendMessage(message->chat->id, "Your name is not provided yet.", false, 0, createBottomKeyboard());
+        }
+
+        bot.getApi().sendMessage(message->chat->id, "Your age: " + to_string(userMap[userId].GetAge()), false, 0, createBottomKeyboard());
+        bot.getApi().sendMessage(message->chat->id, "Do you want to see your password?", false, 0, createInlineYesNoKeyboard());
+        });
+
+    bot.getEvents().onCommand("writefile", [&bot, &userMap](Message::Ptr message) {
+        int userId = message->from->id;
+        string filename = "user_" + to_string(userId) + "_" + message->from->username.c_str() + "_data.txt";
+        ifstream inFile(filename);
+        bool usernameExists = false;
+
+        if (inFile.is_open()) {
+            string line;
+            while (getline(inFile, line)) {
+                if (line.find("TG username: " + message->from->username) != string::npos) {
+                    usernameExists = true;
+                    break;
+                }
+            }
+            inFile.close();
+        }
+
+        if (!usernameExists) {
+            ofstream outFile(filename, ios::app);
+
+            if (outFile.is_open()) {
+                outFile << "Name: " << userMap[userId].GetName() << endl;
+                outFile << "TG username: " << message->from->username << endl;
+                outFile << "Age: " << userMap[userId].GetAge() << endl;
+                outFile << "Password: " << userMap[userId].GetPassword() << endl;
+                outFile << "----------------------" << endl;
+
+                bot.getApi().sendMessage(message->chat->id, "Your data is written.", false, 0, createBottomKeyboard());
+                outFile.close();
+            }
+        }
+        else {
+            bot.getApi().sendMessage(message->chat->id, "Your username is already in the file.", false, 0, createBottomKeyboard());
+        }
+        });
+
+    bot.getEvents().onCommand("links", [&bot](Message::Ptr message) {
+        InlineKeyboardMarkup::Ptr keyboard(new InlineKeyboardMarkup);
+        InlineKeyboardButton::Ptr button(new InlineKeyboardButton);
+
+        button->text = "Github";
+        button->url = "https://github.com/illusiOxd";
+        keyboard->inlineKeyboard.push_back({ button });
+
+        bot.getApi().sendMessage(message->chat->id, "Click that buttons to follow the link.", false, 0, keyboard);
+        });
+
+    try {
         printf("Bot username: %s\n", bot.getApi().getMe()->username.c_str());
         TgLongPoll longPoll(bot);
         printf("Long poll started\n");
-        while (true)
-        {
+        while (true) {
             longPoll.start();
         }
     }
-    catch (TgException& e)
-    {
+    catch (TgException& e) {
         printf("error: %s\n", e.what());
     }
     return 0;
